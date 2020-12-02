bc63521c58b23a34d443dfa4cb86e818f07c6f0e
AizazLarik <73699887+AizazLarik@users.noreply.github.com>
2020-12-02 18:40:56 +0500

    Add files via upload

---
+++ sources_1/new/sine_generator.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/02/2020 04:36:35 PM
+// Design Name: 
+// Module Name: sine_generator
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+//Sine Wave Generator
+module sine_generator ( input clk_in, input rst_in, //clock and reset
+                        input step_in, //trigger a phase step (rate at which you run sine generator)
+                        output wire [7:0] amp_out); //output phase   
+    parameter PHASE_INCR = 32'b1000_0000_0000_0000_0000_0000_0000_0000>>5; //1/64th of 48 khz is 750 Hz
+    reg [7:0] divider;
+    reg [31:0] phase;
+    reg [7:0] amp;
+    assign amp_out = {~amp[7],amp[6:0]};
+    sine_lut lut_1(.clk_in(clk_in), .phase_in(phase[31:26]), .amp_out(amp));
+    
+    always @(posedge clk_in)
+    begin
+        if (rst_in)begin
+            divider <= 8'b0;
+            phase <= 32'b0;
+        end else if (step_in)begin
+            phase <= phase+PHASE_INCR;
+        end
+    end
+endmodule
